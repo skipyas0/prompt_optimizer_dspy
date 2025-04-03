dab927e71ae74922bca445bfa3250d6879750968
skipyas0 <kloudvoj@fel.cvut.cz>
2025-04-03 15:49:33 +0200

    return to dspy, ditch my model_api

--- data.py
+++ data.py
@@ -7,8 +7,6 @@ import logging
 import utils
 import random
 import grading
-from model_api import model
-import my_signatures as sig
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -22,103 +20,8 @@ logger.addHandler(handler)
 
 SOLVE_TEMP = 0.0
 
+
 class Data:
-    def __init__(self, data: list[dict], answer_type: type = str):
-        self.data = data
-        self.train, self.dev, self.test = self.__get_splits()
-        self.scores = {x: [-1]*len(self.__getattribute__(x)) for x in ["train", "dev", "test"]}
-        self.solve_handle = self.__get_solve_handle()
-        self.answer_type = answer_type
-
-    def length(self, split: Literal["all", "train", "dev", "test"] = "all"):
-        return len(self.data) if split == "all" else len(self.__getattribute__(split))
-    
-    def select(self, split: Literal["train", "dev", "test"], n: int) -> list[dict]:
-        assert split in ["train", "dev", "test"]
-        data = self.__getattribute__(split)
-        if n < len(data):
-            return random.sample(data, n)
-        return data
-    
-    def __get_splits(self):
-        ss = len(self.data) // 3
-        return self.data[:ss], self.data[ss:2*ss], self.data[2*ss:]
-
-    def __get_solve_handle(self):
-        #solve_lm = utils.get_lm("SOLVE")
-        
-        #solve_module = dspy.ChainOfThought(grading.str_signature, temperature=SOLVE_TEMP)
-        solve_sig = sig.Signature.from_str(f"task: str (task to be solved) -> solution: {str(self.answer_type)} ()")
-        solve_module = lambda kwargs: model.chain_of_thought(solve_sig, temp=SOLVE_TEMP, **kwargs)
-        def solve(question):
-            try:
-                ret = solve_module(question=question)
-                return ret
-            except Exception as e:
-                return None
-            
-        return solve
-            
-    @classmethod
-    def from_json(cls, path, answer_type):
-        with open(f"{path}", "r") as f:
-            data = json.load(f)
-        return cls(data, answer_type)
-        #def examplify(s):
-        #    ret = dspy.Example(question=s["question"], answer=s["answer"])
-        #    ret.with_inputs("question")
-        #    return ret
-        
-        #data = map(examplify, data)
-        #return cls(list(data))
-
-    def __str__(self):
-        TEMPLATE = "Question: {q}\nAnswer: {a}"
-        example_strings = [TEMPLATE.format(q=e["question"], a=e["answer"]) for e in self.data]
-        return "\n".join(example_strings)
-
-    def eval_on_split(self, 
-                      prompt: Prompt, 
-                      split: Literal["train", "dev", "test"] = "dev",
-                      batch_size: int = 5):
-        assert split in ["train", "dev", "test"]
-        data: list[dict] = self.__getattribute__(split) 
-        if data == None:
-            raise ValueError(f"Wrong split name {split}")
-        
-        if batch_size > 0 and len(data) > batch_size:
-            data = random.sample(data, batch_size)
-
-        
-            
-        old_score = prompt.get_score(split)
-        if old_score > -1.0:
-            logger.warning(f"Tried grading prompt {prompt.text} with assigned score {old_score}.")
-            return old_score
-        if not prompt.valid:
-            logger.warning(f"Tried grading prompt {prompt.text} which is invalid.")
-            prompt.set_score(split, 0.0)
-            return 0.0
-
-        batch_score = 0.0
-        for i, example in enumerate(data):
-            question = prompt.format(example["question"])
-            response = self.solve_handle(question=question)
-            if response:
-                gold = example["answer"]
-                logger.debug(f"Grading problem {i+1} on split {split}")
-                rationale, solution_dict = response
-                solution = solution_dict["solution"]
-                grade = grading.exact_match_float(solution, gold, logger=logger)
-                prompt.completions.append((example, rationale, grade))
-                self.scores[split][i] = grade
-                batch_score += grade
-        prompt.set_score(split, batch_score / len(data))
-        return prompt.get_score(split)
-
-
-
-class DspyData:
     def __init__(self, data: list[dspy.Example]):
         self.data = data
         self.train, self.dev, self.test = self.__get_splits()
